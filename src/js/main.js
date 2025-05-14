1a84790bb77100b1be245c3f1400eff73470c15b
moloko666777 <moloko666777@gmail.com>
2025-05-14 17:39:42 +0300

    init commit

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+import Lenis from 'lenis';
+
+window.addEventListener("DOMContentLoaded", () => {
+    const lenis = new Lenis({
+        duration: 1.2,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        direction: 'vertical',
+        gestureDirection: 'vertical',
+        smooth: true,
+        mouseMultiplier: 1,
+        smoothTouch: false,
+        touchMultiplier: 2,
+        infinite: false,
+    });
+
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    console.log("Lenis initialized");
+});
